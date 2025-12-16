aa8f6825eac82667163c601b584bcbfe8bf4ce53
snappas <snappas@users.noreply.github.com>
2025-12-15 19:41:04 -0600

    renderer: update opengl renderer for moving model code to common

--- src/renderer_vk/tr_local.h
+++ src/renderer_vk/tr_local.h
@@ -1296,7 +1296,6 @@ void        RE_BeginFrame( stereoFrame_t stereoFrame );
 void        RE_BeginRegistration( glconfig_t *glconfig );
 void        RE_LoadWorldMap( const char *mapname );
 void        RE_SetWorldVisData( const byte *vis );
-qhandle_t   RE_RegisterModel( const char *name );
 qhandle_t   RE_RegisterSkin( const char *name );
 void        RE_Shutdown( qboolean destroyWindow );
 
