f69600d03cc59338852f8d2c102f5c67c733fe05
snappas <snappas@users.noreply.github.com>
2026-05-31 12:02:37 -0500

    Update bg_animation.c

--- src/game/bg_animation.c
+++ src/game/bg_animation.c
@@ -277,29 +277,6 @@ static animConditionTable_t animConditionsTable[NUM_ANIM_CONDITIONS] =
 //------------------------------------------------------------
 
 /*
-================
-return a hash value for the given string
-================
-*/
-static long BG_StringHashValue( const char *fname ) {
-	int i;
-	long hash;
-	char letter;
-
-	hash = 0;
-	i = 0;
-	while ( fname[i] != '\0' ) {
-		letter = tolower( fname[i] );
-		hash += (long)( letter ) * ( i + 119 );
-		i++;
-	}
-	if ( hash == -1 ) {
-		hash = 0;   // never return -1
-	}
-	return hash;
-}
-
-/*
 =================
 BG_AnimParseError
 =================
